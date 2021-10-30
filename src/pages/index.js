a7b082a26c19c1c1b78bbae718bfa0902a270d5b
PoetaKodu <rfpawelsyska@gmail.com>
2021-10-30 20:34:36 +0200

    Fixed wrong link.

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ function HomepageHeader() {
 					<div className={styles.buttons}>
 						<Link
 							className="button button--secondary button--lg"
-							to="/learn/index">
+							to="/learn">
 							<Translate>Start learning</Translate>
 						</Link>
 					</div>
