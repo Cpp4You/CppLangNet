1657d03a3d2399afc28dfe28e40c8f2ac4f8613c
PoetaKodu <rfpawelsyska@gmail.com>
2022-01-06 12:26:17 +0100

    🐞 Removed `spacing` prop passed to `div` from `Columns` component

--- src/components/mdx/Columns.js
+++ src/components/mdx/Columns.js
@@ -20,6 +20,7 @@ export default function Columns(props) {
 
 	const divProps = { ...props };
 	delete divProps.columns;
+	delete divProps.spacing;
 
 	return (
 		<div className={styles.columns} {...divProps} style={style}>
