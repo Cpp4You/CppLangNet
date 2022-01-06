768b2cbf3452e35fc1f7b93470915b72da3f5f5d
PoetaKodu <rfpawelsyska@gmail.com>
2022-01-06 12:25:35 +0100

    Fixed `Columns` component props

--- src/components/mdx/Columns.js
+++ src/components/mdx/Columns.js
@@ -18,8 +18,11 @@ export default function Columns(props) {
 		style.gridTemplateColumns = props.columns.map(e => `${e}`).join(" ");
 	}
 
+	const divProps = { ...props };
+	delete divProps.columns;
+
 	return (
-		<div className={styles.columns} {...props} style={style}>
+		<div className={styles.columns} {...divProps} style={style}>
 			{props.children}
 		</div>
 	);
