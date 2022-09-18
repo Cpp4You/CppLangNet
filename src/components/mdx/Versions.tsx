8c1cbd61c4c3a04d0e6bfdff4e13b993fbae13ce
PoetaKodu <rfpawelsyska@gmail.com>
2022-09-18 16:24:13 +0200

    feat: improved versions components

--- src/components/mdx/Versions.tsx
+++ src/components/mdx/Versions.tsx
@@ -1,6 +1,41 @@
-import React from "react";
+import React		from "react";
+import Translate	from "@docusaurus/Translate";
 
-import styles from "./Versions.module.scss";
+import styles		from "./Versions.module.scss";
+
+export const LanguageVersions = new Map<string, React.ReactNode>();
+
+LanguageVersions.set("cpp98", "C++98");
+LanguageVersions.set("cpp03", "C++03");
+LanguageVersions.set("cpp11", "C++11");
+LanguageVersions.set("cpp14", "C++14");
+LanguageVersions.set("cpp17", "C++17");
+LanguageVersions.set("cpp20", "C++20");
+LanguageVersions.set("cpp23", "C++23");
+
+LanguageVersions.set("deprecated-cpp98", <Translate>deprecated in C++98</Translate>);
+LanguageVersions.set("deprecated-cpp03", <Translate>deprecated in C++03</Translate>);
+LanguageVersions.set("deprecated-cpp11", <Translate>deprecated in C++11</Translate>);
+LanguageVersions.set("deprecated-cpp14", <Translate>deprecated in C++14</Translate>);
+LanguageVersions.set("deprecated-cpp17", <Translate>deprecated in C++17</Translate>);
+LanguageVersions.set("deprecated-cpp20", <Translate>deprecated in C++20</Translate>);
+LanguageVersions.set("deprecated-cpp23", <Translate>deprecated in C++23</Translate>);
+
+LanguageVersions.set("until-cpp98", <Translate>until C++98</Translate>);
+LanguageVersions.set("until-cpp03", <Translate>until C++03</Translate>);
+LanguageVersions.set("until-cpp11", <Translate>until C++11</Translate>);
+LanguageVersions.set("until-cpp14", <Translate>until C++14</Translate>);
+LanguageVersions.set("until-cpp17", <Translate>until C++17</Translate>);
+LanguageVersions.set("until-cpp20", <Translate>until C++20</Translate>);
+LanguageVersions.set("until-cpp23", <Translate>until C++23</Translate>);
+
+LanguageVersions.set("since-cpp98", <Translate>since C++98</Translate>);
+LanguageVersions.set("since-cpp03", <Translate>since C++03</Translate>);
+LanguageVersions.set("since-cpp11", <Translate>since C++11</Translate>);
+LanguageVersions.set("since-cpp14", <Translate>since C++14</Translate>);
+LanguageVersions.set("since-cpp17", <Translate>since C++17</Translate>);
+LanguageVersions.set("since-cpp20", <Translate>since C++20</Translate>);
+LanguageVersions.set("since-cpp23", <Translate>since C++23</Translate>);
 
 interface MarkedTextParameters {
 	children: React.ReactNode;
@@ -11,23 +46,38 @@ function MarkedText({children, className}: MarkedTextParameters) {
 	return <span className={styles.markedText + (className ? " " + className : "")}>{children}</span>;
 }
 
-interface MarkedTextWrapperProps {
+interface VersionProps {
 	children: React.ReactNode;
+
+	v: "cpp98" | "cpp03" | "cpp11" | "cpp14" | "cpp17" | "cpp20" | "cpp23";
 }
 
-export function Since({children}: MarkedTextWrapperProps)
+
+export function Since({children, v}: VersionProps)
 {
-	return <MarkedText className={styles.since}>{children} <span className={styles.versionNumber}><small>(since C++17)</small></span></MarkedText>;
+	return (
+		<MarkedText className={styles.since}>{children} 
+			<span className={styles.versionNumber}><small>({LanguageVersions.get(`since-${v}`)})</small>.</span>
+		</MarkedText>
+	);
 }
 
-export function Until({children}: MarkedTextWrapperProps)
+export function Until({children, v}: VersionProps)
 {
-	return <MarkedText className={styles.until}>{children} <span className={styles.versionNumber}><small>(until C++17)</small>.</span></MarkedText>;
+	return (
+		<MarkedText className={styles.until}>{children} 
+			<span className={styles.versionNumber}><small>({LanguageVersions.get(`until-${v}`)})</small>.</span>
+		</MarkedText>
+	);
 }
 
-export function Deprecated({children}: MarkedTextWrapperProps)
+export function Deprecated({children, v}: VersionProps)
 {
-	return <MarkedText className={styles.deprecated}>{children} <span className={styles.versionNumber}><small>(deprecated since C++17)</small>.</span></MarkedText>;
+	return (
+		<MarkedText className={styles.deprecated}>{children} 
+			<span className={styles.versionNumber}><small>({LanguageVersions.get(`deprecated-${v}`)})</small>.</span>
+		</MarkedText>
+	);
 }
 
 
