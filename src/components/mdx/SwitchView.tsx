668e755de22183349ed849125b31c85e105d8f0a
PoetaKodu <rfpawelsyska@gmail.com>
2022-09-18 16:24:13 +0200

    feat: improved versions components

--- src/components/mdx/SwitchView.tsx
+++ src/components/mdx/SwitchView.tsx
@@ -3,37 +3,12 @@ import React from "react";
 // Docusaurus
 import Tabs						from "@theme/Tabs";
 import TabItem					from "@theme/TabItem";
-import Translate				from "@docusaurus/Translate";
+import { LanguageVersions }		from "./Versions";
 
 // Custom
 import transformEmptyTagElem	from "../../helper/TransformEmptyTagElem";
 
 import styles					from "./SwitchView.module.scss"
-
-const langs = new Map<string, React.ReactNode>();
-langs.set("cpp98", "C++98");
-langs.set("cpp03", "C++03");
-langs.set("cpp11", "C++11");
-langs.set("cpp14", "C++14");
-langs.set("cpp17", "C++17");
-langs.set("cpp20", "C++20");
-langs.set("cpp23", "C++23");
-
-langs.set("until-cpp98", <Translate>until C++98</Translate>);
-langs.set("until-cpp03", <Translate>until C++03</Translate>);
-langs.set("until-cpp11", <Translate>until C++11</Translate>);
-langs.set("until-cpp14", <Translate>until C++14</Translate>);
-langs.set("until-cpp17", <Translate>until C++17</Translate>);
-langs.set("until-cpp20", <Translate>until C++20</Translate>);
-langs.set("until-cpp23", <Translate>until C++23</Translate>);
-
-langs.set("since-cpp98", <Translate>since C++98</Translate>);
-langs.set("since-cpp03", <Translate>since C++03</Translate>);
-langs.set("since-cpp11", <Translate>since C++11</Translate>);
-langs.set("since-cpp14", <Translate>since C++14</Translate>);
-langs.set("since-cpp17", <Translate>since C++17</Translate>);
-langs.set("since-cpp20", <Translate>since C++20</Translate>);
-langs.set("since-cpp23", <Translate>since C++23</Translate>);
 
 export default function SwitchView(props) {
 
@@ -45,7 +20,7 @@ export default function SwitchView(props) {
 				(value.value || value.simplified || value.detailed)
 					?
 					(
-						<TabItem value={key} label={langs.get(key)} default={value.default || index == 0}>
+						<TabItem value={key} label={LanguageVersions.get(key)} default={value.default || index == 0}>
 							{value.simplified
 								?
 								(
@@ -67,7 +42,7 @@ export default function SwitchView(props) {
 					)
 					:
 					(
-						<TabItem value={key} label={langs.get(key)} default={index == 0}>
+						<TabItem value={key} label={LanguageVersions.get(key)} default={index == 0}>
 							{transformEmptyTagElem(value)}
 						</TabItem>
 					)
