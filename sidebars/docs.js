5479b0dd9bfd6aaf4901acf50103224c4e781b4b
Daniel Zaradny <danielzaradny@gmail.com>
2022-10-10 03:47:24 +0200

    feat: add sidebar for forward-list

--- sidebars/docs.js
+++ sidebars/docs.js
@@ -577,6 +577,46 @@ module.exports = {
 
 							]
 						},
+						{
+							type: 'category',
+							label: 'Lists',
+							link: { type: 'doc', id: 'std/containers/lists/index' },
+							items: [
+								docsClassCat('forward-list', 'std/containers/lists/forward-list', '', [
+									'constructors',
+									'destructor',
+									'op:operator_assign',
+									'm:assign',
+									'm:get_allocator',
+									cat('Element access'),
+									'm:front',
+									cat('Iterators'),
+									'm:before_begin',
+									'm:begin',
+									'm:end',
+									cat('Capacity'),
+									'm:empty',
+									'm:max_size',
+									cat('Modifiers'),
+									'm:clear',
+									'm:insert_after',
+									'm:emplace_after',
+									'm:erase_after',
+									'm:push_front',
+									'm:emplace_front',
+									'm:pop_front',
+									'm:resize',
+									'm:swap',
+									cat('Operations'),
+									'm:merge',
+									'm:splice_after',
+									'm:remove',
+									'm:reverse',
+									'm:unique',
+									'm:sort',
+								]),
+							]
+						}
 					]
 				},
 				{
