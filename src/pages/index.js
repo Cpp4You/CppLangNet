be95f6cd4e4981e92c08b04ecb79ad1b3c5d2c12
PoetaKodu <poetakodupl@gmail.com>
2022-11-18 17:47:57 +0100

    feat: better styling for tabs (borders, colors, paddings, etc) feat: better styling for the sidebar (background gradient, remove unnecessary text weight) feat: better styling for navbar (gradient, fonts) feat: better styling for text (new font, better paddings, outlines)

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,22 +37,22 @@ function HomepageHeader() {
 
 const Waves = () => {
 	return (
-	<div>
-		<svg className={styles.waves} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-			viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
-			<defs>
-				<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-			</defs>
-			<g className={styles.wavesParallax}>
-				<use xlinkHref="#gentle-wave" x="48" y="0" />
-				<use xlinkHref="#gentle-wave" x="48" y="3" />
-				<use xlinkHref="#gentle-wave" x="48" y="5" />
-				<use xlinkHref="#gentle-wave" x="48" y="7" />
-			</g>
-		</svg>
-	</div>
-	)
-}
+		<div>
+			<svg className={styles.waves} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+				viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+				<defs>
+					<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+				</defs>
+				<g className={styles.wavesParallax}>
+					<use xlinkHref="#gentle-wave" x="48" y="0" />
+					<use xlinkHref="#gentle-wave" x="48" y="3" />
+					<use xlinkHref="#gentle-wave" x="48" y="5" />
+					<use xlinkHref="#gentle-wave" x="48" y="7" />
+				</g>
+			</svg>
+		</div>
+	);
+};
 
 export default function Home() {
 	return (
