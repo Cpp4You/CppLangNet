707c8854ac9e0844ff56d8f6a9d90d1aab965be0
Paweł <rfpawelsyska@gmail.com>
2023-01-13 22:58:37 +0100

    feat(platform): #185 added `FullCode` component

---
+++ src/components/mdx/FullCode.tsx
@@ -0,0 +1,20 @@
+import React		from "react";
+import Translate	from "@docusaurus/Translate";
+import Details		from "@theme/Details";
+
+interface FullCodeProps {
+	content: React.ReactNode;
+}
+
+export default function FullCode({ content }: FullCodeProps) {
+
+	return (
+		<Details summary={<summary><Translate>See full code</Translate></summary>}>
+			<div>
+				{content}
+			</div>
+		</Details>
+	);
+}
+
+FullCode.isMDXComponent = true;
