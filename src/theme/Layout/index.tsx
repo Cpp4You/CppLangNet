3af4ba1b56e132dabb156ae0ecc60470a47688de
PoetaKodu <poetakodupl@gmail.com>
2023-03-18 20:38:22 +0100

    use typescript for swizzled Layout component

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Layout from "@theme-original/Layout";
+import type LayoutType from "@theme/Layout";
+import type {WrapperProps} from "@docusaurus/types";
 
-export default function LayoutWrapper(props) {
+type Props = WrapperProps<typeof LayoutType>;
+
+export default function LayoutWrapper(props: Props): JSX.Element {
 	return (
 		<>
 			<div id="fullpage-background-container">
