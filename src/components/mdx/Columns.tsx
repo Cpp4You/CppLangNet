c2d8f37badd9b1727f877b50cd5cf6547284f7c5
Paweł <rfpawelsyska@gmail.com>
2023-07-06 22:52:12 +0200

    improve(platform): responsiveness of the `Columns` component. (#221)
    <!--
      Thank you for contributing!
    Provide a description of your changes below and a general summary in the
    title.
    -->
    
    ## Description
    
    <!--- Describe your changes in detail here -->
    
    ## Type of Change
    
    <!--- Put an `x` ( and remove spaces ) in all the boxes that apply: -->
    
    ### Changes in docs (`docs`)
    
    - [ ] ✨ `improve(docs)` - e.g. added a new paragraph, example, using a
    better wording, adding a new document, etc.
    - [ ] 🛠️ `fix(docs)` - bug fix, e.g. fix a typo, page render issue
    - [ ] ❌ `BREAKING CHANGE(docs)` - e.g. removing a
    document/article/category that was referenced in many other places
    - [ ] 🧹 `refactor(docs)` - changed a code example, e.g. replaced old
    code with a modern one
    - [ ] 🗑️ `chore(docs)` - other changes that don't affect the docs, e.g.
    updating the CI/CD pipeline
    
    ### Changes in the platform (`platform`)
    
    - [ ] ✨ `feat(platform)` - a new feature, e.g. a new MDX component,
    plugin, theme, etc.
    - [x] 🛠️ `fix(platform)` - bug fix, e.g. fix a typo, issue causing
    component to crash
    - [ ] ❌ `BREAKING CHANGE(platform)` - e.g. removing a feature, changing
    the API, etc.
    - [ ] 🧹 `refactor(platform)` - code improvements, changes, e.g. unifying
    style, renaming internals, etc.
    - [ ] 📝 `docs(platform)` - updated code documentation
    - [ ] 🗑️ `chore(platform)` - other changes that don't affect the
    platform directly, e.g. updating the CI/CD pipeline

--- src/components/mdx/Columns.tsx
+++ src/components/mdx/Columns.tsx
@@ -2,36 +2,37 @@ import React from "react";
 
 import styles from "./Columns.module.scss";
 
+type BreakSize = "xs" | "sm" | "md" | "lg" | "xl";
+
 type ColumsProps = {
-	children: React.ReactNode;
-	style: React.CSSProperties;
-	spacing?: number;
-	columns?: number[];
+  children: React.ReactNode;
+  style: React.CSSProperties;
+  spacing?: number;
+  columns?: number[];
+  breakAt?: BreakSize;
 }
 
-export default function Columns(props: ColumsProps)
-{
-	const style = props.style || {};
+export default function Columns(props: ColumsProps) {
+  const style = props.style || {};
 
-	style.rowGap = props.spacing || 20;	
-	style.columnGap = props.spacing || 20;	
+  style.gap = props.spacing || 20;
 
-	if (!props.columns)
-		style.gridTemplateColumns = "repeat(2, 1fr)";
-	else
-	{
-		style.gridTemplateColumns = props.columns.map(e => `${e}`).join(" ");
-	}
+  if (!props.columns)
+    style.gridTemplateColumns = "repeat(2, 1fr)";
+  else {
+    style.gridTemplateColumns = props.columns.map(e => `${e}`).join(" ");
+  }
 
-	const divProps = { ...props };
-	delete divProps.columns;
-	delete divProps.spacing;
+  const divProps = { ...props };
+  delete divProps.columns;
+  delete divProps.spacing;
+  delete divProps.breakAt;
 
-	return (
-		<div className={styles.columns} {...divProps} style={style}>
-			{props.children}
-		</div>
-	);
+  return (
+    <div className={styles.columns} data-break-at={props.breakAt ?? "md"} {...divProps} style={style}>
+      {props.children}
+    </div>
+  );
 }
 
 Columns.isMDXComponent = true;
