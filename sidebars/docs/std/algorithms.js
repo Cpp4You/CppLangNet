5156765e53447c59f8b2fec401221344e8d8aaac
b4mbus <danielzaradny@gmail.com>
2023-07-22 11:12:44 +0200

    feat: add ordinary stable_partition

--- sidebars/docs/std/algorithms.js
+++ sidebars/docs/std/algorithms.js
@@ -214,6 +214,7 @@ module.exports = {
             docsMethod("std/algo/ordinary/is_partitioned", "since-cpp11"),
             docsMethod("std/algo/ordinary/partition"),
             docsMethod("std/algo/ordinary/partition_copy", "since-cpp11"),
+            docsMethod("std/algo/ordinary/stable_partition", "since-cpp11"),
           ]
         }
       ]
