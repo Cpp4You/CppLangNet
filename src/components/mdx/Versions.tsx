85c7b7e7382602014b9813778afde6eb9ced492a
Oleksandr <126588178+OleksandrVolkodav@users.noreply.github.com>
2023-08-30 12:54:09 +0400

    literals (lang-support) (#227)
    <!--
      Thank you for contributing!
    Provide a description of your changes below and a general summary in the
    title.
    -->
    
    ## Description
    
    <!--- Describe your changes in detail here -->
    
    ## Type of Change
    
    <!--- Put an `x` ( and remove spaces ) in all the boxes that apply: -->
    
    ### Changes in docs (`docs`)
    
    - [x] ✨ `improve(docs)` - e.g. added a new paragraph, example, using a
    better wording, adding a new document, etc.
    - [ ] 🛠️ `fix(docs)` - bug fix, e.g. fix a typo, page render issue
    - [ ] ❌ `BREAKING CHANGE(docs)` - e.g. removing a
    document/article/category that was referenced in many other places
    - [ ] 🧹 `refactor(docs)` - changed a code example, e.g. replaced old
    code with a modern one
    - [ ] 🗑️ `chore(docs)` - other changes that don't affect the docs, e.g.
    updating the CI/CD pipeline
    
    ### Changes in the platform (`platform`)
    
    - [ ] ✨ `feat(platform)` - a new feature, e.g. a new MDX component,
    plugin, theme, etc.
    - [ ] 🛠️ `fix(platform)` - bug fix, e.g. fix a typo, issue causing
    component to crash
    - [ ] ❌ `BREAKING CHANGE(platform)` - e.g. removing a feature, changing
    the API, etc.
    - [ ] 🧹 `refactor(platform)` - code improvements, changes, e.g. unifying
    style, renaming internals, etc.
    - [ ] 📝 `docs(platform)` - updated code documentation
    - [ ] 🗑️ `chore(platform)` - other changes that don't affect the
    platform directly, e.g. updating the CI/CD pipeline

--- src/components/mdx/Versions.tsx
+++ src/components/mdx/Versions.tsx
@@ -12,6 +12,7 @@ LanguageVersions.set("cpp14", "C++14");
 LanguageVersions.set("cpp17", "C++17");
 LanguageVersions.set("cpp20", "C++20");
 LanguageVersions.set("cpp23", "C++23");
+LanguageVersions.set("cpp26", "C++26");
 
 LanguageVersions.set("deprecated-",		 <Translate>deprecated</Translate>);
 LanguageVersions.set("deprecated-cpp98", <Translate>deprecated in C++98</Translate>);
@@ -21,6 +22,7 @@ LanguageVersions.set("deprecated-cpp14", <Translate>deprecated in C++14</Transla
 LanguageVersions.set("deprecated-cpp17", <Translate>deprecated in C++17</Translate>);
 LanguageVersions.set("deprecated-cpp20", <Translate>deprecated in C++20</Translate>);
 LanguageVersions.set("deprecated-cpp23", <Translate>deprecated in C++23</Translate>);
+LanguageVersions.set("deprecated-cpp26", <Translate>deprecated in C++26</Translate>);
 
 LanguageVersions.set("until-cpp98", <Translate>until C++98</Translate>);
 LanguageVersions.set("until-cpp03", <Translate>until C++03</Translate>);
@@ -29,6 +31,7 @@ LanguageVersions.set("until-cpp14", <Translate>until C++14</Translate>);
 LanguageVersions.set("until-cpp17", <Translate>until C++17</Translate>);
 LanguageVersions.set("until-cpp20", <Translate>until C++20</Translate>);
 LanguageVersions.set("until-cpp23", <Translate>until C++23</Translate>);
+LanguageVersions.set("until-cpp26", <Translate>until C++26</Translate>);
 
 LanguageVersions.set("since-cpp98", <Translate>since C++98</Translate>);
 LanguageVersions.set("since-cpp03", <Translate>since C++03</Translate>);
@@ -37,6 +40,7 @@ LanguageVersions.set("since-cpp14", <Translate>since C++14</Translate>);
 LanguageVersions.set("since-cpp17", <Translate>since C++17</Translate>);
 LanguageVersions.set("since-cpp20", <Translate>since C++20</Translate>);
 LanguageVersions.set("since-cpp23", <Translate>since C++23</Translate>);
+LanguageVersions.set("since-cpp26", <Translate>since C++26</Translate>);
 
 LanguageVersions.set("removed-",	  <Translate>deleted</Translate>);
 LanguageVersions.set("removed-cpp98", <Translate>removed in C++98</Translate>);
@@ -46,6 +50,7 @@ LanguageVersions.set("removed-cpp14", <Translate>removed in C++14</Translate>);
 LanguageVersions.set("removed-cpp17", <Translate>removed in C++17</Translate>);
 LanguageVersions.set("removed-cpp20", <Translate>removed in C++20</Translate>);
 LanguageVersions.set("removed-cpp23", <Translate>removed in C++23</Translate>);
+LanguageVersions.set("removed-cpp26", <Translate>removed in C++26</Translate>);
 
 interface MarkedTextParameters {
 	children: React.ReactNode;
@@ -59,7 +64,7 @@ function MarkedText({children, className}: MarkedTextParameters) {
 interface VersionProps {
 	children: React.ReactNode;
 
-	v: "cpp98" | "cpp03" | "cpp11" | "cpp14" | "cpp17" | "cpp20" | "cpp23";
+	v: "cpp98" | "cpp03" | "cpp11" | "cpp14" | "cpp17" | "cpp20" | "cpp23" | "cpp26";
 }
 
 
