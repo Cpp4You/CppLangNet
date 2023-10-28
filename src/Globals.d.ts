0160eef7f16ad5c5cee46d5eb968712fce926b93
PoetaKodu <poetakodupl@gmail.com>
2023-10-28 13:53:00 +0200

    fix: declare classes on `.css` and `.scss` modules

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,8 +1,20 @@
-declare module "*.module.css";
-declare module "*.module.scss";
+// Images
 declare module "*.png";
 declare module "*.svg";
 
+// Styles
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+
+// Raw files
 declare module "!!raw-loader!*" {
   const contents: string;
   export = contents;
