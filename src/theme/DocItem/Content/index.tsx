0e8a47802eea1c1822db6f554c9eac7b32859347
PoetaKodu <poetakodupl@gmail.com>
2023-10-28 21:19:24 +0200

    fix: invalid name imported from Docusaurus

--- src/theme/DocItem/Content/index.tsx
+++ src/theme/DocItem/Content/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Content from "@theme-original/DocItem/Content";
 import type ContentType from "@theme/DocItem/Content";
 import type { WrapperProps } from "@docusaurus/types";
-import { useDocInternal } from "@docusaurus/theme-common/internal";
+import { useDoc as useDocInternal } from "@docusaurus/theme-common/internal";
 
 import DocSettings from "@site/src/components/DocSettings";
 import CppRefAttribution from "@site-comps/CppRefAttribution";
