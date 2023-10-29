ea5d5fcf35eaf03a47ac8ba8042718dd422a1280
PoetaKodu <poetakodupl@gmail.com>
2023-10-29 14:22:28 +0100

    refactor: reformat code

--- src/components/mdx/Image.tsx
+++ src/components/mdx/Image.tsx
@@ -1,94 +1,92 @@
-import React			from "react";
-import useBaseUrl		from "@docusaurus/useBaseUrl";
+import React from "react";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 import transformEmptyTagElem from "../../helper/TransformEmptyTagElem";
 
 type ImageProps = {
-	desc?: React.ReactNode;
-	center?: boolean;
-	fullwidth?: boolean;
-	border?: boolean;
-	style?: React.CSSProperties;
-	
-	borderRadius: number;
-	minwidth?: number;
-	width?: number;
-	maxwidth?: number;
-	minheight?: number;
-	height?: number;
-	maxheight?: number;
+  desc?: React.ReactNode;
+  center?: boolean;
+  fullwidth?: boolean;
+  border?: boolean;
+  style?: React.CSSProperties;
 
-	src?: string;
+  borderRadius: number;
+  minwidth?: number;
+  width?: number;
+  maxwidth?: number;
+  minheight?: number;
+  height?: number;
+  maxheight?: number;
+
+  src?: string;
 }
 
-export default function Image(props: ImageProps)
-{
-	// Setup description
-	let desc = null;
-	if (props.desc)
-	{
-		const descStyle: React.CSSProperties =
-			props.center ? {
-				textAlign: "center",
-				display: "block"
-			} : {};
-			
-		desc = <small style={descStyle}>{transformEmptyTagElem(props.desc)}</small>;
-	}
+export default function Image(props: ImageProps) {
+  // Setup description
+  let desc = null;
+  if (props.desc) {
+    const descStyle: React.CSSProperties =
+      props.center ? {
+        textAlign: "center",
+        display: "block"
+      } : {};
 
-	const fullWidth = props.fullwidth || props.center;
+    desc = <small style={descStyle}>{transformEmptyTagElem(props.desc)}</small>;
+  }
 
-	const style: React.CSSProperties = {
-		border:			(props.border		? "1px solid gray"		: undefined),
-		display:		(fullWidth			? "block"				: "inline-block"),
-	};
+  const fullWidth = props.fullwidth || props.center;
 
-	const imgStyle = props.style || {};
-	imgStyle.borderRadius	= props.borderRadius	|| undefined;
-	imgStyle.minWidth		= props.minwidth		|| undefined;
-	imgStyle.width			= props.width			|| undefined;
-	imgStyle.maxWidth		= props.maxwidth		|| undefined;
-	imgStyle.minHeight		= props.minheight		|| undefined;
-	imgStyle.height			= props.height 			|| undefined;
-	imgStyle.maxHeight		= props.maxheight		|| undefined;
-	imgStyle.display		= "block";
-	// Temp Fix: blurry images on Chrome
-	imgStyle.imageRendering = "-webkit-optimize-contrast";
+  const style: React.CSSProperties = {
+    border: (props.border ? "1px solid gray" : undefined),
+    display: (fullWidth ? "block" : "inline-block"),
+  };
 
-	if (props.center)
-		imgStyle.margin = "0 auto";
+  const imgStyle = props.style || {};
+  imgStyle.borderRadius = props.borderRadius || undefined;
+  imgStyle.minWidth = props.minwidth || undefined;
+  imgStyle.width = props.width || undefined;
+  imgStyle.maxWidth = props.maxwidth || undefined;
+  imgStyle.minHeight = props.minheight || undefined;
+  imgStyle.height = props.height || undefined;
+  imgStyle.maxHeight = props.maxheight || undefined;
+  imgStyle.display = "block";
+  // Temp Fix: blurry images on Chrome
+  imgStyle.imageRendering = "-webkit-optimize-contrast";
 
-	const processedProps = {...props};
+  if (props.center)
+    imgStyle.margin = "0 auto";
 
-	if (typeof props.src === "string")
-		processedProps.src = props.src.startsWith("/img/") ? useBaseUrl(props.src) : props.src;
+  const processedProps = { ...props };
+
+  if (typeof props.src === "string")
+    processedProps.src = props.src.startsWith("/img/") ? useBaseUrl(props.src) : props.src;
 
 
-	delete processedProps.border;
-	delete processedProps.borderRadius;
-	delete processedProps.center;
-	delete processedProps.fullwidth;
-	delete processedProps.style;
+  delete processedProps.border;
+  delete processedProps.borderRadius;
+  delete processedProps.center;
+  delete processedProps.fullwidth;
+  delete processedProps.style;
 
-	delete processedProps.minwidth;
-	delete processedProps.width;
-	delete processedProps.maxwidth;
+  delete processedProps.minwidth;
+  delete processedProps.width;
+  delete processedProps.maxwidth;
 
-	delete processedProps.minheight;
-	delete processedProps.height;
-	delete processedProps.maxheight;
+  delete processedProps.minheight;
+  delete processedProps.height;
+  delete processedProps.maxheight;
 
-	delete processedProps.desc;
+  delete processedProps.desc;
 
 
-	return (
-		<>
-			<div style={style}>
-				<img {...processedProps} style={imgStyle}/>
-			</div>
-			{desc}
-		</>
-	);
+  return (
+    <>
+      <div style={style}>
+        <img {...processedProps} style={imgStyle} />
+      </div>
+      {desc}
+    </>
+  );
 }
 
 Image.isMDXComponent = true;
--- src/components/mdx/SwitchView.tsx
+++ src/components/mdx/SwitchView.tsx
@@ -1,52 +1,52 @@
 import React from "react";
 
 // Docusaurus
-import Tabs						from "@theme/Tabs";
-import TabItem					from "@theme/TabItem";
-import { LanguageVersions }		from "./Versions";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
+import { LanguageVersions } from "./Versions";
 
 // Custom
-import transformEmptyTagElem	from "../../helper/TransformEmptyTagElem";
+import transformEmptyTagElem from "../../helper/TransformEmptyTagElem";
 
-import styles					from "./SwitchView.module.scss"
+import styles from "./SwitchView.module.scss";
 
 export default function SwitchView(props) {
 
-	const content = props.content || { };
+  const content = props.content || {};
 
-	return (
-		<Tabs>
-			{Object.entries(content).map(([key, value], index) => (
-				(value.value || value.simplified || value.detailed)
-					?
-					(
-						<TabItem value={key} label={LanguageVersions.get(key)} default={value.default || index == 0}>
-							{value.simplified
-								?
-								(
-									<>
-										<Tabs groupId="view_mode" className={styles.smallTabs}>
-											<TabItem value="simplified" label="Simplified" default>
-												{transformEmptyTagElem(value.simplified)}
-											</TabItem>
-											<TabItem value="detailed" label="Detailed">
-												{transformEmptyTagElem(value.detailed)}
-											</TabItem>
-										</Tabs>
-									</>
-								)
-								:
-								value.value
-							}
-						</TabItem>
-					)
-					:
-					(
-						<TabItem value={key} label={LanguageVersions.get(key)} default={index == 0}>
-							{transformEmptyTagElem(value)}
-						</TabItem>
-					)
-			))}
-		</Tabs>
-	);
+  return (
+    <Tabs>
+      {Object.entries(content).map(([key, value], index) => (
+        (value.value || value.simplified || value.detailed)
+          ?
+          (
+            <TabItem value={key} label={LanguageVersions.get(key)} default={value.default || index == 0}>
+              {value.simplified
+                ?
+                (
+                  <>
+                    <Tabs groupId="view_mode" className={styles.smallTabs}>
+                      <TabItem value="simplified" label="Simplified" default>
+                        {transformEmptyTagElem(value.simplified)}
+                      </TabItem>
+                      <TabItem value="detailed" label="Detailed">
+                        {transformEmptyTagElem(value.detailed)}
+                      </TabItem>
+                    </Tabs>
+                  </>
+                )
+                :
+                value.value
+              }
+            </TabItem>
+          )
+          :
+          (
+            <TabItem value={key} label={LanguageVersions.get(key)} default={index == 0}>
+              {transformEmptyTagElem(value)}
+            </TabItem>
+          )
+      ))}
+    </Tabs>
+  );
 }
