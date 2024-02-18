440c06d3f92eab48a260c4f49b24fb853d36147b
PoetaKodu <poetakodupl@gmail.com>
2024-02-18 17:15:22 +0100

    refactor: `SwitchView` component

--- src/components/mdx/SwitchView.tsx
+++ src/components/mdx/SwitchView.tsx
@@ -21,6 +21,7 @@ type SwitchViewProps = {
   content: Record<string, SwitchViewContent | React.ReactNode>;
 }
 
+
 export default function SwitchView(props: SwitchViewProps) {
 
   const content = props.content || {};
@@ -28,55 +29,54 @@ export default function SwitchView(props: SwitchViewProps) {
   return (
     <Tabs>
       {Object.entries(content).map(([key, value], index) => (
-        <SwitchViewItem key={key} value={value} index={index}/>
+        renderSwitchViewItem(key, index, value)
       ))}
     </Tabs>
   );
 }
 
-type SwitchViewItemProps = {
-  key: string;
-  index: number;
-  value: SwitchViewContent | React.ReactNode;
-}
+type SwitchViewItemValue = SwitchViewContent | React.ReactNode;
 
-function SwitchViewItem({key, index, value}: SwitchViewItemProps) {
-
-  if (typeof value === "object" && ("value" in value || "simplified" in value || "detailed" in value))
-  {
-    return (
-      <TabItem value={key}
-        label={LanguageVersions.get(key)}
-        default={value.default || index == 0}
-      >
-        {value.simplified
-          ?
-          (
-            <>
-              <Tabs groupId="view_mode" className={styles.smallTabs}>
-                <TabItem value="simplified" label="Simplified" default>
-                  {transformEmptyTagElem(value.simplified)}
-                </TabItem>
-                <TabItem value="detailed" label="Detailed">
-                  {transformEmptyTagElem(value.detailed)}
-                </TabItem>
-              </Tabs>
-            </>
-          )
-          :
-          value.value
-        }
-      </TabItem>
-    );
-  }
+/**
+ * **⚠️ Important**  
+ * This function isn't meant to be used as a React component.
+ * Use a call instead, because in Docusaurus internals, <Tabs> content is
+ * [sanitized](https://github.com/facebook/docusaurus/blob/1bbc68bc30d017158bb352cf25552b4791799619/packages/docusaurus-theme-common/src/utils/tabsUtils.tsx#L64)
+ * and it explicitly checks the type of children before rendering.
+ */
+function renderSwitchViewItem(key: string, index: number, value: SwitchViewItemValue) {
+  const explicitDefaultOption = (typeof value === "object" && "default" in value) ? value.default : null;
+  const defaultOption = explicitDefaultOption ?? index === 0;
 
   return (
     <TabItem
       value={key}
-      label={LanguageVersions.get(key)}
-      default={index == 0}
+      label={LanguageVersions.get(key) || key}
+      default={defaultOption}
     >
-      {transformEmptyTagElem(value)}
+      {isSwitchViewContent(value) ? renderSwitchViewContent(value) : transformEmptyTagElem(value)}
     </TabItem>
+  );
+}
+
+function isSwitchViewContent(value: any): value is SwitchViewContent {
+  return typeof value === "object" && ("value" in value || "simplified" in value || "detailed" in value);
+}
+
+function renderSwitchViewContent(value: SwitchViewContent) {
+
+  if (!value.simplified) {
+    return value.value;
+  }
+
+  return (
+    <Tabs groupId="view_mode" className={styles.smallTabs}>
+      <TabItem value="simplified" label="Simplified" default>
+        {transformEmptyTagElem(value.simplified)}
+      </TabItem>
+      <TabItem value="detailed" label="Detailed">
+        {transformEmptyTagElem(value.detailed)}
+      </TabItem>
+    </Tabs>
   );
 }
